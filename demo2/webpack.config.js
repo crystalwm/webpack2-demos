b0f00bcda5c89247bfc1db8cef86244d1f3b720d
crystalwm <1207356200@qq.com>
2017-09-26 17:24:03 +0800

    demo2

--- demo2/webpack.config.js
+++ demo2/webpack.config.js
@@ -2,16 +2,20 @@ var path = require('path');
 var webpack = require('webpack');
 var options = {
     devtool: 'sourcemap'
-}
+};
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
-    entry: './index.js',
+    entry: './demo2/index.js',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
     plugins: [
-        new webpack.optimize.UglifyJsPlugin({
+        /*         new webpack.optimize.UglifyJsPlugin({
+                    sourceMap: options.devtool && (options.devtool.indexOf("sourcemap") >= 0 || options.devtool.indexOf("source-map") >= 0)
+                }) */
+        new UglifyJSPlugin({
             sourceMap: options.devtool && (options.devtool.indexOf("sourcemap") >= 0 || options.devtool.indexOf("source-map") >= 0)
         })
     ]
--- docs/demo2.md
+++ docs/demo2.md
@@ -6,3 +6,6 @@
 ```
 webpack 
 ```
+
+**UglifyjsWebpackPlugin和webpack.optimize.UglifyJsPlugin的关系**
+webpack contains the same plugin under webpack.optimize.UglifyJsPlugin
