70bc18a8bfe4324b7620191fffe9f13be4efd3e5
crystalwm <1207356200@qq.com>
2018-05-22 13:34:29 +0800

    uglifyis-webpack-plugin

--- demo2/webpack.config.js
+++ demo2/webpack.config.js
@@ -7,6 +7,7 @@ const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
     entry: './demo2/index.js',
+    mode: 'development',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
